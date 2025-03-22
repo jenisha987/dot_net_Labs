b9e10f02290380f75a0f962b710d3b921dd5d0bb
jenisha987 <jensastha987@gmail.com>
2025-03-22 10:53:40 +0545

    added files in new branch

---
+++ Lab_1/add.py
@@ -0,0 +1,10 @@
+def add(n1, n2):
+    print("****ADD****")
+    return n1 + n2
+
+print("Enter two numbers:")
+a = int(input("n1:"))
+b = int(input("n2:"))
+result = add(a, b)
+print(f"The sum of {a} and {b} is: {result}")
+    
