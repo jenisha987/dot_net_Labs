a6c371b16ab2c69fdba638bf1067bb8ca1016a55
jenisha987 <jensastha987@gmail.com>
2025-03-27 09:56:30 +0545

    lab2 initiated and programmed helloname print

---
+++ lab2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using lab2;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        print_helloname obj = new print_helloname();
+        obj.display();
+    }
+}
---
+++ lab2/print_helloname.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace lab2
+{
+    public class print_helloname
+    {
+        public void display()
+        {
+            Console.WriteLine("Write a C# program to print Hello and your name in a separate line.");
+            Console.WriteLine("Hello");
+            Console.WriteLine("My name is Jenisha Shrestha");
+        }
+    }
+}
